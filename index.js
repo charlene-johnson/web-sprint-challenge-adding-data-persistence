7483b34c71a39f9756f9e49e8c564e42d18dd6b9
charlene-johnson <charlene.j8234@gmail.com>
2020-08-28 19:20:14 -0400

    Added the basefiles and installed dependencies

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const helmet = require("helmet")
+
+
+const server = express()
+const port = process.env.PORT || 5000
+
+server.use(helmet())
+server.use(express.json())
+
+server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+        message: "Sorry, something went wrong, try again later",
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Running at http://localhost:${port}`)
+})
