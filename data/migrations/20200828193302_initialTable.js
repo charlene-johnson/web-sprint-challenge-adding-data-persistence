b1a489b6b85774b7641643cfd7af9a503b91c2fc
charlene-johnson <charlene.j8234@gmail.com>
2020-08-28 19:51:41 -0400

    Wrote out my migration tables

--- data/migrations/20200828193302_initialTable.js
+++ data/migrations/20200828193302_initialTable.js
@@ -1,8 +1,37 @@
 
 exports.up = async function(knex) {
-  
+  await knex.schema.createTable("projects", (table) => {
+      table.increments("id")
+      table.text("name").notNull()
+      table.text("description")
+      table.boolean("complete").notNull().defaultTo(false)
+  })
+  await knex.schema.createTable("tasks", (table) => {
+      table.increments("id")
+      table.text("description").notNull()
+      table.text("notes")
+      table.boolean("complete").notNull().defaultTo(false)
+      table.integer("project_id").notNull().references("id").inTable("projects").onDelete("CASCADE").onUpdate("CASCADE")
+  })
+  await knex.schema.createTable("resources", (table) => {
+      table.increments("id")
+      table.text("name").notNull()
+      table.text("description")
+  })
+  await knex.schema.createTable("project_tasks", (table) => {
+      table.integer("project_id").notNull().references("id").inTable("projects").onDelete("CASCADE").onUpdate("CASCADE")
+      table.integer("task_id").notNull().references("id").inTable("tasks").onDelete("CASCADE").onUpdate("CASCADE")
+  })
+  await knex.schema.createTable("project_resources", (table) => {
+    table.integer("project_id").notNull().references("id").inTable("projects").onDelete("CASCADE").onUpdate("CASCADE")
+    table.integer("resource_id").notNull().references("id").inTable("resources").onDelete("CASCADE").onUpdate("CASCADE")
+  })
 }
 
 exports.down = async function(knex) {
-  
+    await knex.schema.dropTableIfExists("project_resources")
+    await knex.schema.dropTableIfExists("project_tasks")
+    await knex.schema.dropTableIfExists("resources")
+    await knex.schema.dropTableIfExists("tasks")
+    await knex.schema.dropTableIfExists("projects")
 }
